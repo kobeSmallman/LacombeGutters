aac65abc878e518047fada666c6fd64d80ea1dcb
Kobe Smallman <kobe4smallman@gmail.com>
2026-03-12 14:01:10 -0600

    Increase spacing between bullet list and Get Quote on 6-inch card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -287,7 +287,7 @@ export default function Home() {
                       Designed for larger roofs and heavy rainfall areas, our 6&quot; oversized gutters provide superior water handling capacity to protect your home&apos;s foundation.
                     </p>
                     
-                    <div className="space-y-3 mb-10">
+                    <div className="space-y-3 mb-12">
                       <div className="flex items-center">
                         <svg className="h-5 w-5 text-blue-600 mr-2" fill="currentColor" viewBox="0 0 20 20">
                           <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
